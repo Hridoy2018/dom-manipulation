a55d1484c0d2d0419635051ed90dd7440d2846c6
Hridoy2018 <hridoyhridoy19@yahoo.com>
2018-05-30 05:02:09 +0000

    task 2a 2b 2c

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ var heading = document.getElementById('heading')
 console.log(heading.innerText)
 /* Task 2 using plain JavaScript */
 
+var textBtn = document.getElementById('textBtn2')
+textBtn.addEventListener('click',function(){
+    var task2 = document.getElementById('task2a')
+    var paragraph =document.createElement('p')
+    paragraph.innerText ="Hello world"
+    task2.appendChild(paragraph)
+})
 
+//Task2b
+function changeBgcolor(event){
+    var body = document.getElementsByTagName('body')[0]
+    if (event.target.innerText == "Red"){
+        body.style.backgroundColor ='red'
+    }
+    if(event.target.innerText =="Green"){
+        body.style.backgroundColor = 'green'
+    }
+}
+//task 2c
+function redirectToGoogle(){
+    window.location.assign('https://www.google.com')
+}
 
 /* Task 4 using jQuery */
